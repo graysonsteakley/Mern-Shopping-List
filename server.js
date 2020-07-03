ec2245ebf2d216c52a06a7eb645a3c6f4d82df50
Gray Steakley <grayson.steakley@gmail.com>
2020-07-02 23:41:33 -0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use('/api/items', items);
 
 //SERVE static assets if in production
 
-if(process.event.NODE_ENV === 'production'){
+
     app.use(express.static('client/build'));
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+
 
 
 const port = process.env.PORT || 5000;
