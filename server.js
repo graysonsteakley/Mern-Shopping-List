c4e10065a8088e8ba295a507b400c709823d9f1c
Gray Steakley <grayson.steakley@gmail.com>
2020-07-02 23:34:10 -0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/items', items);
 
 //SERVE static assets if in production
 
-if(process.event.NODE_ENV = 'production'){
+if(process.event.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
